6db77bc54a5df25247c1779da76f31dbedcae154
John <johnisaacarthur@gmail.com>
2024-11-04 10:48:08 -0700

    number guesser

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response;
+        do {
+            int gcount = 0;
+            int mgs = 1;
+            int mnum = 2;
+            Console.WriteLine("Welcome to the guessing game!");
+            Console.WriteLine("Try to guess what I am thinking");
+            while(mgs != mnum) {
+                if(gcount == 0) {
+                    Random randomGenerator = new Random();
+                    int number = randomGenerator.Next(1,52);
+                }
+                Console.Write("Guess a number: ");
+                Console.ReadLine();
+                gcount ++;
+                if(mgs > mnum) {
+                    Console.WriteLine("Lower");
+                }
+                else if(mgs < mnum) {
+                    Console.WriteLine("Higher");
+                }
+                else {
+                    Console.WriteLine("Correct!");
+                    Console.WriteLine($"It took you {gcount} guesses to guess correctly");
+                }
+            }
+            Console.Write("Would you like to play again?(y/n) ");
+            response = Console.ReadLine();
+        }while(response == "y");
+        Console.WriteLine("Thanks for playing!");
     }
 }
