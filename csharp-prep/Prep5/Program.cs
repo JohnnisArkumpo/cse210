5ed38907cb9f5da928f13cd59ab6f06bf8ab7fde
John <johnisaacarthur@gmail.com>
2024-11-06 10:33:07 -0700

    Prep assignment 5 done

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
+    static void displayWelcome() {
+        Console.WriteLine("Welcome to the program! ");
+    }
+    static string promptUserName() {
+        Console.Write("What's your name? ");
+        return Console.ReadLine();
+    }
+    static int promptUserNumber() {
+        Console.Write("What's your favorite number? ");
+        string tempn = Console.ReadLine();
+        return int.Parse(tempn);
+    }
+    static int squareNumber(int n) {
+        return n*n;
+    }
+    static void displayResult(string nm, int nb) {
+        Console.WriteLine($"{nm}, The square of your number is {nb}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        displayWelcome();/**
+        string un = promptUserName();
+        int unum = promptUserNumber();
+        int unumsq = squareNumber(unum);
+        displayResult(un, unumsq);**/
+        displayResult(promptUserName(), squareNumber(promptUserNumber()));
     }
 }
