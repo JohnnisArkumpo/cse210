63793512325810cf43c1373322bc1473e0627404
John <johnisaacarthur@gmail.com>
2024-11-16 19:17:04 -0700

    worked on program, verse, and scripture classes. Began functionallity testing

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,10 +1,23 @@
 using System;
 
 class Program {
+    static string chooseScripture() {
+        Console.Write("We will be memorizing scriptures from 2 Nephi 2\nChoose a verse or a set of verses from this chapter: ");
+        string userChoice = Console.ReadLine();
+        Verse memorizationToday = new Verse();
+        string versestring = memorizationToday.getPassage(userChoice);
+        return versestring;
+    }
+    static void scriptureDisplay(string scrip) {
+        Console.Clear();
+        Console.WriteLine(scrip);
+    }
     static void Main(string[] args)
     {
         // Console.WriteLine("Hello Develop03 World!");
-        
+        /*create a display function
+        create a getInput function
+        utilize the other three classes*/
     }
 }
 
--- prove/Develop03/verse.cs
+++ prove/Develop03/verse.cs
@@ -2,10 +2,10 @@ using System;
 
 class Verse {
     // start
-    private string passageContent = "And my father dwelt in a tent";
-    private string getPassage(string locator) {
+    private string passageContent = "Behold, he offereth himself a sacrifice for sin, to answer the ends of the law, unto all those who have a broken heart and a contrite spirit; and unto none else can the ends of the law be answered.";
+    public string getPassage(string locator) {
         if(locator == "true") {
-            passageContent = "scripture.location.content";
+            passageContent = "Scripture chosenScripture = new Scripture();\nforeach(i in chosenScripture.passages) {\nif(chosenScripture.passages[i] == locator) {\nreturn chosenScripture.passages[i];\n}\n}";// incomplete creation of usage for multiple scripture possibilities
         }
         return passageContent;
     }
