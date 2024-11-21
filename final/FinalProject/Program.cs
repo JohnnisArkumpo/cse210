7bacddda7e0e2a5f7534fc55ab3ff1fc04b0c351
John <johnisaacarthur@gmail.com>
2024-11-21 11:16:54 -0700

    Re-started with correct classes

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         // Console.WriteLine("Hello FinalProject World!");
+        
     }
 }
+
+/**
+    FIVE CLASSES
+    Pattern - Abstract or interface
+    Piece
+    Player
+    Board
+    Effect - Abstract or interface
+**/
